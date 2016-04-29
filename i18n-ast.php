14a0f4ea21cdc732c591ac5b221f47e540b73154
Antonio Jesús Sánchez Padial <antonio@spadial.com>
2016-04-30 00:24:49 +0200

    Añadíu esbozu de la tradución

---
+++ i18n-ast.php
@@ -0,0 +1,17 @@
+<?php return array(
+    'ast'    => array(
+        'snippet_proposal_title' => '<snippet_undefined>A <snippet_keyword>%1%</snippet_keyword> fálten-y pasos. Defínilos con estos pasos:</snippet_undefined>',
+        'snippet_missing_title'  => '<snippet_undefined>Les plantíes pa los siguientes pasos en <snippet_keyword>%1%</snippet_keyword> nun fueron xeneraes (revisa la to configuración):</snippet_undefined>',
+        'failed_scenarios_title' => 'Casos fallíos:',
+        'failed_hooks_title'     => 'Hooks fallíos:',
+        'failed_steps_title'     => 'Pasos fallíos:',
+        'pending_steps_title'    => 'Pasos pendientes:',
+        'scenarios_count'        => '{0} Nengún casu|{1} 1 casu|]1,Inf] %1% casos',
+        'steps_count'            => '{0} Nengún pasu|{1} 1 pasu|]1,Inf] %1% pasos',
+        'passed_count'           => '[1,Inf] %1% pasaron',
+        'failed_count'           => '[1,Inf] %1% fallaron',
+        'pending_count'          => '[1,Inf] %1% empatanaes',
+        'undefined_count'        => '[1,Inf] %1% ensin definir',
+        'skipped_count'          => '[1,Inf] %1% saltaes',
+    ),
+);
